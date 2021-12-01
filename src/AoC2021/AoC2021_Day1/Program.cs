b0d783ef845dad867d4620580928856160156a79
jesper <jesper.svensson@biltema.com>
2021-12-01 10:39:13 +0100

    day1 task 1
    all the linqs, made it as convoluted as possible

---
+++ src/AoC2021/AoC2021_Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+
+"input.txt"
+    .Map(PathToFile)
+    .Map(fp => new Counter(fp))
+    .Tee(ReadFile)
+    .Map(SetFirstValue)
+    .Tee(LoopRowsWithCalc)
+    .Map(c => c.Increases)
+    .Print();
+
+Counter ReportSet(Counter c, int curr, string msg) =>
+    curr.Tee(p => c.PrevDepth = p)
+        .Map(p => $"{p}({msg})")
+        .Print()
+        .Map(p => c);
+
+string PathToFile(string filename) =>
+    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
+
+void ReadFile(Counter counter) =>
+    counter.Rows = counter.FilePath.Map(File.ReadAllLines).Select(int.Parse).ToList();
+
+Counter SetFirstValue(Counter c) =>
+    ReportSet(c, c.Rows.First(), "N / A - no previous measurement");
+
+void LoopRowsWithCalc(Counter c) =>
+    c.Rows.ForEach(r => Calc(c, r));
+
+Counter Calc(Counter c, int curr) =>
+    curr > c.PrevDepth ?
+        ReportSet(c, curr, "increased").Tee(ct => ct.Increases++) :
+        ReportSet(c, curr, "decreased");
+
+public class Counter
+{
+    public string FilePath { get; }
+
+    public Counter(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    public int Increases { get; set; }
+    public int PrevDepth { get; set; }
+    public List<int> Rows { get; set; }
+}
---
+++ src/AoC2021/Common/FunctionalExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Common;
+
+/// <summary>Functional extensions for generic service work.</summary>
+public static class FunctionalExtensions
+{
+    /// <summary>Runs work fn with service TSource returning TResult.</summary>
+    /// <typeparam name="TSource">Service</typeparam>
+    /// <typeparam name="TResult">Result</typeparam>
+    /// <param name="this">Service invoker</param>
+    /// <param name="fn">Work</param>
+    public static TResult Map<TSource, TResult>(this TSource @this, Func<TSource, TResult> fn)
+        => fn(@this);
+
+    /// <summary>Runs work fn with service TSource returning the same service.</summary>
+    public static T Tee<T>(this T @this, Action<T> fn)
+    {
+        fn(@this);
+        return @this;
+    }
+}
+
+public static class Printer
+{
+    public static object Print(this object o)
+    {
+        Console.WriteLine($"{o}");
+        return o;
+    }
+}
