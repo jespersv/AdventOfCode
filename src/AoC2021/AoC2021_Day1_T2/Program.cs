1472df7b45bf60f6e365121eb72fadd0fe5ca666
jesper <jesper.svensson@biltema.com>
2021-12-01 16:40:11 +0100

    day1 task 2

--- src/AoC2021/AoC2021_Day1_T2/Program.cs
+++ src/AoC2021/AoC2021_Day1_T2/Program.cs
@@ -9,7 +9,6 @@
     .Map(c => c.Increases)
     .Print();
 
-
 string PathToFile(string filename) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
 
 List<int> ReadFile(string filePath) => filePath.Map(File.ReadAllLines).Select(int.Parse).ToList();
@@ -17,17 +16,20 @@ List<int> ReadFile(string filePath) => filePath.Map(File.ReadAllLines).Select(in
 Counter NewCounter(List<int> rows) => new(rows);
 
 Counter SetFirstValue(Counter c) =>
-    ReportSet(c, c.Rows.First(), "N / A - no previous measurement");
+    ReportSet(c, c.Rows.Take(3).Sum(), "N / A - no previous measurement");
 
 void LoopRowsWithCalc(Counter c) =>
-    c.Rows.ForEach(r => Calc(c, r));
+    c.Rows
+        .Where((v, indx) => indx != 0 && indx < c.Rows.Count - 1)
+        .Select((v, indx) => c.Rows.Skip(indx + 1).Take(3).Sum()).ToList()
+        .ForEach(r => Calc(c, r));
 
 Counter Calc(Counter c, int curr) =>
-    curr > c.PrevDepth ?
-        ReportSet(c, curr, "increased").Tee(ct => ct.Increases++) :
-        ReportSet(c, curr, "decreased");
-
-
+    curr == c.PrevDepth ?
+        ReportSet(c, curr, "no change") :
+        curr > c.PrevDepth ?
+            ReportSet(c, curr, "increased").Tee(ct => ct.Increases++) :
+            ReportSet(c, curr, "decreased");
 
 Counter ReportSet(Counter c, int curr, string msg) =>
     curr.Tee(c.SetPreviousValue)
